ab05a7c01b803d86320f7a4a15445bb22a592458
zhangyuyang <yuyang.zhang@tongdun.net>
2023-07-21 13:31:24 +0800

    add day06

---
+++ demo_code/src/com/wy/leetcode/practise_30/Day06.java
@@ -0,0 +1,10 @@
+package com.wy.leetcode.practise_30;
+
+/**
+ * @author yuyang.zhang
+ * @Description
+ * @date 2023/7/21
+ */
+public class Day06 {
+
+}
---
+++ demo_code/src/com/wy/leetcode/practise_30/TreeNode.java
@@ -0,0 +1,28 @@
+package com.wy.leetcode.practise_30;
+
+/**
+ * @author yuyang.zhang
+ * @Description
+ * @date 2023/7/21
+ */
+public class TreeNode {
+    private int val;
+    private TreeNode left;
+
+    private TreeNode right;
+
+    public TreeNode() {
+        this.val = val;
+    }
+
+
+
+    public TreeNode init(Integer ...values) {
+        TreeNode treeNode = new TreeNode();
+        for (Integer value : values) {
+
+        }
+
+        return treeNode;
+    }
+}
