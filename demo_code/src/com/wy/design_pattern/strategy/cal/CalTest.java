fc04877764040a649e12202cdfb4bdca943ab2fe
HelloWorldDoNotLaugh <yuyang.zhang@tongdun.net>
2023-09-05 10:21:22 +0800

    add CalTest.java

--- demo_code/src/com/wy/design_pattern/strategy/cal/CalTest.java
+++ demo_code/src/com/wy/design_pattern/strategy/cal/CalTest.java
@@ -1,6 +1,8 @@
 package com.wy.design_pattern.strategy.cal;
 
 import java.math.BigDecimal;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author yuyang.zhang
@@ -8,12 +10,12 @@ import java.math.BigDecimal;
  * @date 2023/8/21
  */
 public class CalTest {
+    private static final Pattern PROPERTY_PATTERN = Pattern.compile("^[a-z][a-z0-9_]{0,49}$");
     public static void main(String[] args) throws Exception{
-        BigDecimal a = new BigDecimal("3");
-        BigDecimal b = new BigDecimal("5");
-
-        BigDecimal add = Calculate.cal(a, b, "div");
-
-        System.out.println(add.toString());
+        String value = "id";
+        Matcher matcher = PROPERTY_PATTERN.matcher(value);
+        if (!matcher.matches()) {
+            System.out.println(matcher.toString());
+        }
     }
 }
