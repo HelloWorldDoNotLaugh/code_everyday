393227aa49895a533aaf08d427c8fab9c6b53a5e
zhangyuyang <helloworld.dng@gmail.com>
2023-09-07 11:00:25 +0800

    modify day20 todo

--- demo_code/src/com/wy/leetcode/practise_30/Day20.java
+++ demo_code/src/com/wy/leetcode/practise_30/Day20.java
@@ -18,6 +18,16 @@ public class Day20 {
      * @return com.wy.leetcode.tree.TreeNode
      *
      *    preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+     *
+     *    根据前序遍历的结果可以先确定  3 一定是根节点。
+     *    再到中序遍历中可以确定 左子树 9， 右子树 15， 20，7
+     *
+     *    这个例子中整棵树的左子树就只有 9，所以左子树✅
+     *
+     *    对于右子树 15 20 7
+     *    依据前序遍历：20是根节点，再根据中序确定 15在左子树，7在右子树。
+     *
+     *    整体思路还是递归，先找根节点，构建左子树，右子树
      */
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         return null;
