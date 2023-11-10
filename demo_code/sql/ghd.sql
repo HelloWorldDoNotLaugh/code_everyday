8ea98302bda5a9cc7c577d94d83efe3db1d1d9b0
helloworld <helloworld.dng@gmail.com>
2023-11-11 06:23:27 +0800

    add 1111

---
+++ demo_code/sql/ghd.sql
@@ -0,0 +1,14 @@
+
+# 查找用select, * 代表查询全部, from 后跟要查询对表名, where 后接查询条件
+# 条件查询
+# 1. 等于: =
+# 2. 不等于:!=
+# 3. 大于: >
+# 4. 小于: <
+# 5. 大于等于: >=
+# 6. 小于等于: <=
+# 7. 包含: like
+# 8. 不包含: not like
+# 9. 开始于: like 'xxx%'
+# 10. 结束于: like '%xxx'
+select * from tableName where id = '';
---
+++ demo_code/src/com/wy/leetcode/L765.java
@@ -0,0 +1,10 @@
+package com.wy.leetcode;
+
+public class L765 {
+    public int minSwapsCouples(int[] row) {
+        //todo 并查集
+
+
+        return 0;
+    }
+}
